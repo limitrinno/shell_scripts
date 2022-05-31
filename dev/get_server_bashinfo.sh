3ca84e9238e8d11eda627206fd210b37d45a3f5b
limitrinno <limitrinno@gmail.com>
2022-05-31 16:30:17 +0800

    Update

--- dev/get_server_bashinfo.sh
+++ dev/get_server_bashinfo.sh
@@ -40,6 +40,8 @@ server_cpu_1min_load=`uptime | grep -o "load average.*" | cut -d':' -f 2 | cut -
 server_cpu_5min_load=`uptime | grep -o "load average.*" | cut -d':' -f 2 | cut -d',' -f 2 | sed 's/ //g'`
 ## 系统15分钟负载
 server_cpu_15min_load=`uptime | grep -o "load average.*" | cut -d':' -f 2 | cut -d',' -f 3 | sed 's/ //g'`
+# CPU核心
+server_cpu_number=`cat /proc/cpuinfo| grep "physical id"| sort| uniq| wc -l`
 
 # 内存指标
 server_mem_total=`free | grep "Mem:" | awk '{print $2}'`
@@ -48,6 +50,25 @@ server_swap_total=`free | grep "Swap:" | awk '{print $2}'`
 server_swap_use=`free | grep "Swap:" | awk '{print $3}'`
 
 # 网络指标
+echo "" > /tmp/netpush.txt && sed -i '1d' /tmp/netpush.txt
+i=0
+while ((++i));
+do
+network_card=`ip ad|awk '/state UP/ {print $2}' | sed -n "$i"p | sed 's/://g'`
+if [[ $network_card == "" ]];then
+        break
+fi
+#echo -e "流量进入--流量传出    "
+old_in=$(cat /proc/net/dev |grep $network_card |awk '{print $2}')
+old_out=$(cat /proc/net/dev |grep $network_card |awk '{print $10}')
+sleep 2
+new_in=$(cat /proc/net/dev |grep $network_card |awk '{print $2}')
+new_out=$(cat /proc/net/dev |grep $network_card |awk '{print $10}')
+in=$(printf "%.1f%s" "$((($new_in-$old_in)/1024))")
+out=$(printf "%.1f%s" "$((($new_out-$old_out)/1024))")
+echo "server_network_in{network_device='"$network_card"',network_in='"$in"'} $in" >> /tmp/netpush.txt
+echo "server_network_out{network_device='"$network_card"',network_out='"$out"'} $out" >> /tmp/netpush.txt
+done
 
 # 容量指标
 echo "" > /tmp/push.txt && sed -i '1d' /tmp/push.txt
@@ -64,7 +85,12 @@ fi
 echo "server_df_info{server_df_device='"$server_df_device"',server_df_mountpoint='"$server_df_mountpoint"'} $server_df_use" >> /tmp/push.txt
 done
 
-# 上传服务器
+# 上传网络数据
+sed -i s#\'#\"#g /tmp/netpush.txt
+curl -XPOST --data-binary @/tmp/netpush.txt http://$szsrv:49091/metrics/job/$job_name/instance/$instance_name
+rm -rf /tmp/netpush.txt
+
+# 上传硬盘数据
 sed -i s#\'#\"#g /tmp/push.txt
 curl -XPOST --data-binary @/tmp/push.txt http://$szsrv:49091/metrics/job/$job_name/instance/$instance_name
 rm -rf /tmp/push.txt
@@ -79,6 +105,7 @@ server_mem_total $server_mem_total
 server_mem_use $server_mem_use
 server_swap_total $server_swap_total
 server_swap_use $server_swap_use
+server_cpu_number $server_cpu_number
 EOF
 
 # END
